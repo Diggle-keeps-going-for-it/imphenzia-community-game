52b96954304408cc05c839d24c66f5d196a72a63
Anthony Diggle <anthonydiggle@carbidefunction.co.uk>
2024-10-19 20:48:49 +0100

    Add character controller from store asset, modified by us in Farm Up!

---
+++ Assets/TopDownShooterController/Media/Scripts/Feature/ActivateSkills/HandleDoubleJumpSkill.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    public class HandleDoubleJumpSkill : MonoBehaviour
+    {
+        [Header("Activate doblejump")] [Tooltip("Enable player doblejump if true, disable if false")]
+        public bool ActivateDoubleJump;
+
+        public bool DestroyIfActive;
+
+        public GameObject Effect;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+
+            if (Effect)
+            {
+                Instantiate(Effect, transform.position, transform.rotation);
+            }
+
+            if (DestroyIfActive)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/TopDownShooterController/Media/Scripts/PlayerControllers/MovementCharacterController.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    public class MovementCharacterController : MonoBehaviour
+    {
+        [Header("Player Controller Settings")] [Tooltip("Speed for the player.")]
+        public float RunningSpeed = 5f;
+
+        [Header("Speed when player is shooting")] [Range(0.2f, 1)] [Tooltip("This is the % from player normal speed.")]
+        public float RunningShootSpeed;
+
+        [Tooltip("Slope angle limit to slide.")]
+        public float SlopeLimit = 45;
+
+        [Tooltip("Slide friction.")] [Range(0.1f, 0.9f)]
+        public float SlideFriction = 0.3f;
+
+        [Tooltip("Gravity force.")] [Range(0, -100)]
+        public float Gravity = -30f;
+
+        [Tooltip("Maxima speed for the player when fall.")] [Range(0, 100)]
+        public float MaxDownYVelocity = 15;
+
+        [Tooltip("Can the user control the player?")]
+        public bool CanControl = true;
+
+        [Tooltip("This is the drag force for the character, a standard value are (8, 0, 8). ")]
+        public Vector3 DragForce;
+
+        [Tooltip("This is the animator for you character.")]
+        public Animator PlayerAnimator;
+
+        [Header("Effects")] [Tooltip("This position is in the character feet and is use to instantiate effects.")]
+        public Transform LowZonePosition;
+
+        public PlayerController PlayerController;
+
+        //Input.
+        public float Horizontal;
+        public float Vertical;
+        public float Horizontal2;
+        public float Vertical2;
+
+        //private vars
+        private CharacterController _controller;
+        private Vector3 _velocity;
+
+        //get direction for the camera
+        private Transform _cameraTransform;
+        private Vector3 _forward;
+        private Vector3 _right;
+
+        //temporal vars
+        private float _originalRunningSpeed;
+        private float _gravity;
+        private bool _invertedControl;
+        private Vector3 _hitNormal;
+        private Vector3 _move;
+
+        public bool IsMoving => _move != Vector3.zero;
+
+        private void Awake()
+        {
+            _controller = GetComponent<CharacterController>();
+            _originalRunningSpeed = RunningSpeed;
+        }
+
+        private void Start()
+        {
+            if (Camera.main != null) _cameraTransform = Camera.main.transform;
+            _gravity = Gravity;
+        }
+
+        private void Update()
+        {
+            //capture input from direct input
+            //this is for normal movement
+            Horizontal = PlayerController.GetHorizontalValue();
+            Vertical = PlayerController.GetVerticalValue();
+
+            //this invert controls 
+            if (_invertedControl)
+            {
+                Horizontal *= -1;
+                Vertical *= -1;
+            }
+
+            //if player can control the character
+            if (!CanControl)
+            {
+                Horizontal = 0;
+                Vertical = 0;
+            }
+
+            //set running animation
+            SetRunningAnimation((Math.Abs(Horizontal) > 0 || Math.Abs(Vertical) > 0));
+        }
+
+        private void FixedUpdate()
+        {
+            //get the input direction for the camera position.
+            _forward = _cameraTransform.TransformDirection(Vector3.forward);
+            _forward.y = 0f;
+            _forward = _forward.normalized;
+            _right = new Vector3(_forward.z, 0.0f, -_forward.x);
+
+            _move = (Horizontal * _right + Vertical * _forward);
+
+            _move.Normalize();
+
+            //move the player if no is active the slow fall(this avoid change the speed for the fall)
+            if (_controller.enabled)
+            {
+                _controller.Move(Time.deltaTime * RunningSpeed * _move);
+            }
+
+            if (_move != Vector3.zero)
+            {
+                transform.forward = Vector3.Lerp(transform.forward, _move, 0.6f);
+            }
+
+            //gravity force
+            if (_velocity.y >= -MaxDownYVelocity)
+            {
+                _velocity.y += Gravity * Time.deltaTime;
+            }
+
+            _velocity.x /= 1 + DragForce.x * Time.deltaTime;
+            _velocity.y /= 1 + DragForce.y * Time.deltaTime;
+            _velocity.z /= 1 + DragForce.z * Time.deltaTime;
+
+            if (_controller.enabled)
+            {
+                _controller.Move(_velocity * Time.deltaTime);
+            }
+        }
+
+        //This check how much the player are pushing the fire stick
+        public bool TensionFoRightStickLowerThan(float value)
+        {
+            return (Mathf.Abs(PlayerController.MovCharController.Horizontal2) > value ||
+                    Mathf.Abs(PlayerController.MovCharController.Vertical2) > value);
+        }
+
+        public void ResetOriginalSpeed()
+        {
+            RunningSpeed = _originalRunningSpeed;
+        }
+
+        //change the speed for the player
+        public void ChangeSpeed(float speed)
+        {
+            RunningSpeed = speed;
+        }
+
+        //change the speed for the player for a time period
+        public void ChangeSpeedInTime(float speedPlus, float time)
+        {
+            StartCoroutine(ModifySpeedByTime(speedPlus, time));
+        }
+
+        //invert player control(like a confuse skill)
+        public void InvertPlayerControls(float invertTime)
+        {
+            //check if not are already inverted
+            if (!_invertedControl)
+            {
+                StartCoroutine(InvertControls(invertTime));
+            }
+        }
+
+        private void OnControllerColliderHit(ControllerColliderHit hit)
+        {
+            _hitNormal = hit.normal;
+        }
+        //Animation
+
+        #region Animator
+
+        private void SetRunningAnimation(bool run)
+        {
+            PlayerAnimator.SetBool("Running", run);
+        }
+
+        #endregion
+
+        #region Coroutine
+
+        //Use this to deactivate te player control for a period of time.
+        public IEnumerator DeactivatePlayerControlByTime(float time)
+        {
+            _controller.enabled = false;
+            CanControl = false;
+            yield return new WaitForSeconds(time);
+            CanControl = true;
+            _controller.enabled = true;
+        }
+
+        //dash coroutine.
+        private IEnumerator Dashing(float time)
+        {
+            CanControl = false;
+            if (!_controller.isGrounded)
+            {
+                Gravity = 0;
+                _velocity.y = 0;
+            }
+
+            //animate hear to true
+            yield return new WaitForSeconds(time);
+            CanControl = true;
+            //animate hear to false
+            Gravity = _gravity;
+        }
+
+        //modify speed by time coroutine.
+        private IEnumerator ModifySpeedByTime(float speedPlus, float time)
+        {
+            if (RunningSpeed + speedPlus > 0)
+            {
+                RunningSpeed += speedPlus;
+            }
+            else
+            {
+                RunningSpeed = 0;
+            }
+
+            yield return new WaitForSeconds(time);
+            RunningSpeed = _originalRunningSpeed;
+        }
+
+        private IEnumerator InvertControls(float invertTime)
+        {
+            yield return new WaitForSeconds(0.1f);
+            _invertedControl = true;
+            yield return new WaitForSeconds(invertTime);
+            _invertedControl = false;
+        }
+
+        #endregion
+    }
+}
