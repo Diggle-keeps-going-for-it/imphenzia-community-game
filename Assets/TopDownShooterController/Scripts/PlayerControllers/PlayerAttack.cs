013ea43219bfe9dfb3ac5e53d253f1da3af69ac7
Anthony Diggle <anthonydiggle@carbidefunction.co.uk>
2024-10-20 21:48:47 +0100

    Separate the animation into different sections so I can do different gameplay things during them
    Currently the windup and the cast are interchangeable in gameplay, but I am considering adding hold-to-ready, then release to attack. Possibly could add hurt can interrupt windup but not cast too.

--- Assets/TopDownShooterController/Scripts/PlayerControllers/PlayerAttack.cs
+++ Assets/TopDownShooterController/Scripts/PlayerControllers/PlayerAttack.cs
@@ -7,7 +7,16 @@ public class PlayerAttack : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private string animatorStartAttackTriggerName;
-    [SerializeField] private float attackDuration;
+    [SerializeField] private string animatorAttackTimeParameterName;
+    [SerializeField] private string animatorEndAttackTriggerName;
+
+    [SerializeField] [Min(0f)] private float windupDuration;
+    [SerializeField] [Min(0f)] private float castDuration;
+    [SerializeField] [Tooltip("How long to play the end of the cast animation for. Does not affect gameplay")] [Min(0f)] private float recoveryAnimationDuration;
+    [SerializeField] [Tooltip("How long after firing before the next shot can be started")] [Min(0f)] private float recoveryDuration;
+
+    [SerializeField] [Range(0f, 1f)] private float animationStartCastNormalizedTime;
+    [SerializeField] [Range(0f, 1f)] private float animationAttackExtremeNormalizedTime;
 
     private bool isAttacking = false;
 
@@ -21,12 +30,50 @@ public class PlayerAttack : MonoBehaviour
 
     private IEnumerator Attack()
     {
-        Debug.Log("Attacking");
         isAttacking = true;
         animator.SetTrigger(animatorStartAttackTriggerName);
+        var startTime = Time.time;
 
-        yield return new WaitForSeconds(attackDuration);
-
+        yield return PlayWindup(startTime);
+        yield return PlayCast(startTime);
+        var proportionOfRecoveryThatIsUninterruptible = recoveryDuration / recoveryAnimationDuration;
+        var animationUninterruptibleRecoveryNormalizedTime = Mathf.Lerp(animationAttackExtremeNormalizedTime, 1f, proportionOfRecoveryThatIsUninterruptible);
+        yield return PlayUninterruptibleRecovery(startTime, animationUninterruptibleRecoveryNormalizedTime);
         isAttacking = false;
+        animator.SetTrigger(animatorEndAttackTriggerName);
+        yield return PlayPostAttackRecoveryGameplay(startTime, animationUninterruptibleRecoveryNormalizedTime);
+    }
+
+    private IEnumerator PlayWindup(float startTime)
+    {
+        yield return PlayAnimatedAttackSection(startTime, windupDuration, 0f, animationStartCastNormalizedTime);
+    }
+
+    private IEnumerator PlayCast(float startTime)
+    {
+        yield return PlayAnimatedAttackSection(startTime + windupDuration, castDuration, animationStartCastNormalizedTime, animationAttackExtremeNormalizedTime);
+    }
+
+    private IEnumerator PlayUninterruptibleRecovery(float startTime, float animationUninterruptibleRecoveryNormalizedTime)
+    {
+        yield return PlayAnimatedAttackSection(startTime + windupDuration + castDuration, recoveryDuration, animationAttackExtremeNormalizedTime, animationUninterruptibleRecoveryNormalizedTime);
+    }
+
+    private IEnumerator PlayPostAttackRecoveryGameplay(float startTime, float animationUninterruptibleRecoveryNormalizedTime)
+    {
+        yield return PlayAnimatedAttackSection(startTime + windupDuration + castDuration + recoveryDuration, recoveryAnimationDuration - recoveryDuration, animationUninterruptibleRecoveryNormalizedTime, 1f);
+    }
+
+    private IEnumerator PlayAnimatedAttackSection(float sectionStartTime, float sectionDuration, float inAnimationStartNormalizedTime, float inAnimationEndNormalizedTime)
+    {
+        var sectionEndTime = sectionStartTime + sectionDuration;
+        while (Time.time < sectionEndTime)
+        {
+            var timeSinceStart = Time.time - sectionStartTime;
+            var inSectionProportion = timeSinceStart / sectionDuration;
+            var inAnimationTime = Mathf.Lerp(inAnimationStartNormalizedTime, inAnimationEndNormalizedTime, inSectionProportion);
+            animator.SetFloat(animatorAttackTimeParameterName, inAnimationTime);
+            yield return 0;
+        }
     }
 }
